b1dd72b9fe5e0fd754db28058e9f8f64d158d545
OmarJaff <me@omarjaff.com>
2023-11-22 16:44:07 +0300

    extended Tailwind Colors with custom color system

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require('tailwindcss/colors');
 
 
 /** @type {import('tailwindcss').Config} */
@@ -8,7 +9,27 @@ export default {
 		extend: {
 			fontFamily: {
 				sans: ["LabelGrotesk", ...defaultTheme.fontFamily.sans],
-			}
+			},
+			colors: {
+				'primary-black': '#0A0909',
+                'primary-black-base': '#030303',
+                'primary-black-800': '#151414',
+				'primary-black-700': '#1F1E1E',
+				'primary-gray-600': '#282828',
+				'primary-gray-500': '#424242',
+				'primary-gray-400': '#525252',
+				'primary-gray-300': '#858585',
+				'primary-gray-200':'#DEDEDE',
+				'primary-gray-100':'#EDEDED',
+				'primary-gray-50':'#F9F9F9',
+				'success': '#29AE29',
+				'warning': '#FFC62A',
+				'error': '#EF4444',
+				'orange-base': '#ED7615',
+				'lavender-base': '#6E3CF5',
+				'Sussie-base': '#38E1F1',
+				'blue-jeans': '#0082FF'
+			},
 		},
 	},
 	plugins: [],
