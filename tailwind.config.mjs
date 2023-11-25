c2afd1c3e352de3970e8671b5b94e4e2e434eb25
OmarJaff <me@omarjaff.com>
2023-11-25 18:46:46 +0300

    added button background custom color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -15,6 +15,7 @@ export default {
                 'primary-black-base': '#030303',
                 'primary-black-800': '#151414',
 				'primary-black-700': '#1F1E1E',
+				'primary-black-400': '#141414',
 				'primary-gray-600': '#282828',
 				'primary-gray-500': '#424242',
 				'primary-gray-400': '#525252',
