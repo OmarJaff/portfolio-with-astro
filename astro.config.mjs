9604d4ef322d64668c52939300d7ab17d2b4606a
OmarJaff <me@omarjaff.com>
2023-12-26 16:51:33 +0300

    Made configuration for keystatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import alpinejs from "@astrojs/alpinejs";
+import react from "@astrojs/react";
+import keystatic from "@keystatic/astro";
+
+import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()],
-  site: "http://localhost:4321/",
+  output: "hybrid",
+  integrations: [tailwind(), alpinejs(), react(), keystatic(), markdoc()],
+  site: "http://example.com"
 });
