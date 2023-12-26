f8ac70fc3e52846c715578289c4aa337a623ed83
OmarJaff <me@omarjaff.com>
2023-12-26 16:06:47 +0300

    Added configuration for RSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,5 @@ import alpinejs from "@astrojs/alpinejs";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), alpinejs()],
+  site: "http://localhost:4321/",
 });
