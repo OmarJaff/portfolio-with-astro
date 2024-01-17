9dcff0c87143f49d18fab919dcf0c0f767cd2d8f
OmarJaff <me@omarjaff.com>
2024-01-18 00:39:37 +0300

    Added netlify adabter to enalbe SSR in the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 import react from "@astrojs/react";
 import keystatic from "@keystatic/astro";
-
 import markdoc from "@astrojs/markdoc";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   integrations: [tailwind(), alpinejs(), react(), keystatic(), markdoc()],
   site: "http://example.com",
+  adapter: netlify()
 });
