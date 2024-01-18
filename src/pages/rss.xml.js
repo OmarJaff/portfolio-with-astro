c72fdfa3350a735eb5f720485ddd6054ce85de7d
OmarJaff <me@omarjaff.com>
2024-01-18 11:01:41 +0300

    Adjusted rss detail

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,7 +3,7 @@ import rss, { pagesGlobToRssItems } from "@astrojs/rss";
 export async function GET(context) {
   return rss({
     title: "My Articles | Blog",
-    description: "My Articles",
+    description: "My Articles, written by Omar Jaff",
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
     customData: `<language>en-us</language>`,
