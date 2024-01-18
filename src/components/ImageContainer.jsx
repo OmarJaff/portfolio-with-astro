171d6ec32877fe461d5657a58c30acf19d717dab
OmarJaff <me@omarjaff.com>
2024-01-18 16:54:50 +0300

    Created React component for project images

---
+++ src/components/ImageContainer.jsx
@@ -0,0 +1,49 @@
+import ReactDOM from 'react-dom';
+
+import { animate, scroll } from "motion";
+
+ 
+scroll(
+  animate(
+    "#projectimg",
+    {
+      filter: [
+        "grayscale(100%)",
+        "grayscale(100%)",
+        "grayscale(0%)",
+        "grayscale(0%)",
+        "grayscale(0%)",
+        "grayscale(100%)",
+        "grayscale(100%)",
+      ],
+    },
+    {
+      easing: ["ease-in", "linear", "ease-out"],
+    }
+  ),
+  {
+    offset: ["start start", "end end"],
+  }
+);
+
+class ImageContainer extends React.Component {
+    
+    render() {
+      return  <div class="group relative">
+      <img
+        id="projectimg"
+        alt={this.props.title}
+        width="3648"
+        height="3117"
+        decoding="async"
+        class="w-full"
+        sizes="(min-width: 1216px) 76rem, 100vw"
+        src={this.props.src}
+      />
+</div>;
+    }
+  }
+
+  
+
+  export default ImageContainer;
