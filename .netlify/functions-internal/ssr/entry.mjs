1fc3ee4920a4db96e7be23f5e0307cb599dd7d06
OmarJaff <me@omarjaff.com>
2025-03-06 02:01:42 +0300

    Removed bg color

--- .netlify/functions-internal/ssr/entry.mjs
+++ .netlify/functions-internal/ssr/entry.mjs
@@ -1,18 +1,18 @@
 import * as adapter from '@astrojs/netlify/ssr-function.js';
 import { renderers } from './renderers.mjs';
-import { manifest } from './manifest_KOXex1Ii.mjs';
+import { manifest } from './manifest_e7KXclO_.mjs';
 
 const _page0  = () => import('./chunks/keystatic-api_H2YJAARz.mjs');
-const _page1  = () => import('./chunks/keystatic-astro-page_Xa_8nJ0s.mjs');
-const _page2  = () => import('./chunks/generic_VzT_j_bj.mjs');
-const _page3  = () => import('./chunks/index_2ik6-VtI.mjs');
-const _page4  = () => import('./chunks/_.._mBUnlrym.mjs');
-const _page5  = () => import('./chunks/articles__PFj230d.mjs');
-const _page6  = () => import('./chunks/_.._8-BWzvyl.mjs');
-const _page7  = () => import('./chunks/projects_wmL1xWu2.mjs');
-const _page8  = () => import('./chunks/rss_Ev45xbHs.mjs');
-const _page9  = () => import('./chunks/about_Bty46Kd4.mjs');
-const _page10  = () => import('./chunks/404_vZUaPvp-.mjs');const pageMap = new Map([["node_modules/@keystatic/astro/internal/keystatic-api.js", _page0],["node_modules/@keystatic/astro/internal/keystatic-astro-page.astro", _page1],["node_modules/astro/dist/assets/endpoint/generic.js", _page2],["src/pages/index.astro", _page3],["src/pages/articles/[...slug].astro", _page4],["src/pages/articles.astro", _page5],["src/pages/projects/[...slug].astro", _page6],["src/pages/projects.astro", _page7],["src/pages/rss.xml.js", _page8],["src/pages/about.astro", _page9],["src/pages/404.astro", _page10]]);
+const _page1  = () => import('./chunks/keystatic-astro-page_XA47G3dV.mjs');
+const _page2  = () => import('./chunks/generic_Ui30WqTP.mjs');
+const _page3  = () => import('./chunks/index_DuYlQUpv.mjs');
+const _page4  = () => import('./chunks/_.._Ne0bgT4W.mjs');
+const _page5  = () => import('./chunks/articles_B2eMR1e3.mjs');
+const _page6  = () => import('./chunks/_.._ln9rfKH-.mjs');
+const _page7  = () => import('./chunks/projects_ggy3uAJ1.mjs');
+const _page8  = () => import('./chunks/rss_e18lU-sO.mjs');
+const _page9  = () => import('./chunks/about_04v0FNA1.mjs');
+const _page10  = () => import('./chunks/404_tGgWDSNh.mjs');const pageMap = new Map([["node_modules/@keystatic/astro/internal/keystatic-api.js", _page0],["node_modules/@keystatic/astro/internal/keystatic-astro-page.astro", _page1],["node_modules/astro/dist/assets/endpoint/generic.js", _page2],["src/pages/index.astro", _page3],["src/pages/articles/[...slug].astro", _page4],["src/pages/articles.astro", _page5],["src/pages/projects/[...slug].astro", _page6],["src/pages/projects.astro", _page7],["src/pages/rss.xml.js", _page8],["src/pages/about.astro", _page9],["src/pages/404.astro", _page10]]);
 const _manifest = Object.assign(manifest, {
 	pageMap,
 	renderers,
